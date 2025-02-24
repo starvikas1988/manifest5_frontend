1cac3a25af2b9ff31b1ca990cbe21e9b123fe113
starvikas1988 <smartvikass1988@gmail.com>
2025-02-24 12:50:28 +0530

    dashboard data added

--- src/components/Dashboard1.js
+++ src/components/Dashboard1.js
@@ -2,26 +2,12 @@ import React from "react";
 import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import DatePicker from "react-datepicker";
-import SearchIcon from '../images/dashboard_main/Group 2555.png'
-import GroupIcon from '../images/dashboard_main/Group 1830.png'
-import CardIcon from '../images/dashboard_main/Group 1821.png'
-import Ticket from '../images/dashboard_main/Group 1829.png'
-import DateIcon from '../images/dashboard_main/Frame 1966.png'
-
-import img1 from '../images/dashboard_main/B (1).png'
-import img2 from '../images/dashboard_main/H (1).png'
-import img3 from '../images/dashboard_main/Group 1823.png'
-import img4 from '../images/dashboard_main/arrow_down.png'
-
 import "react-datepicker/dist/react-datepicker.css";
 import axiosInstance from "../utils/axiosInstance"; // Adjust path as needed
 import ApiAuthProvider from "../utils/AuthProvider";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import "../styles/Dashboard.css";
-import "../styles/SearchHeader.css";
-import "../styles/Card.css";
-import { px } from "framer-motion";
 
 const Dashboard = () => {
   const [users, setUsers] = useState([]);
@@ -256,73 +242,7 @@ const Dashboard = () => {
               </div>
             </div>
 
-             <div className="box-search" >
-                <div className="content" style={{marginTop:"0px"}}>
-                    {/* <!-- Series Search Bar --> */}
-                    <div className="field-container" style={{width: "30%", paddingLeft:"0px"}} >
-                    <span className="field-name-input">SERIES</span>
-                    <div className="search-bar series-search" style={{bottom : "-11px"}}>
-                        <input type="text"/>
-                        <img style={{width:"55px",right: "1px"}} src={SearchIcon} alt="Down Arrow"/>
-                    </div>
-                    </div>
-                
-                    {/* <!-- Match Format Search Bar --> */}
-                    <div className="field-container" style={{width: "25%"}}>
-                    <span className="field-name-input" >MATCH FORMAT</span>
-                    <div className="search-bar match-search" style={{bottom : "-11px"}}>
-                    {
-                      <select
-                        value={selectedMatch}
-                        onChange={(e) => setSelectedMatch(e.target.value)}
-                        className="dropdown-select"
-                      >
-                        <option value="">Select an option</option>
-                        {options.map((option, index) => (
-                          <option key={index} value={option}>
-                            {option}
-                          </option>
-                        ))}
-                      </select>
-                    }
-                        <img style={{width:"55px",right: "1px"}} src={SearchIcon} alt="Down Arrow"/>
-                    </div>
-                    </div>
-                
-                    {/* <!-- Date Picker --> */}
-                    <div className="field-container" style={{width: "16%"}}>
-                    <span className="field-name-input" >DATE</span>
-                    <div className="date-picker" style={{bottom : "-11px"}}>
-                        <DatePicker
-                            ref={datePickerRef}
-                            selected={selectedStartDate}
-                            onChange={(date) => setSelectedStartDate(date)}
-                            dateFormat="yyyy-MM-dd" // Customize date format as needed
-                            className="date-picker-input" // Add a class for styling
-                            placeholderText="Select a date"
-                        />
-                        <img style={{width:"55px",right: "-12px"}} src={DateIcon} alt="Calendar Icon"/>
-                    </div>
-                    </div>
-                
-                    {/* <!-- List Section --> */}
-                    <div className="field-container">
-                        <span className="field-name-input">LIST<br/>(50)</span>
-                        <img src={GroupIcon} alt="List Icon"/>
-                    </div>
-
-                    {/* <!-- Card Section --> */}
-                    <div className="field-container">
-                        <span className="field-name-input">CARD<br/>(50)</span>
-                        <img src={CardIcon} alt="Card Icon"/>
-                    </div>
-                    <div className="field-container">
-                        <span className="field-name-input">RAISE<br/> TICKET</span>
-                        <img src={Ticket} alt="List Icon"/>
-                    </div>
-                </div>
-            </div>    
-            {/* <div className="box">
+            <div className="box">
               <div className="content" style={{ marginTop: "0px" }}>
                 <div className="field-container">
                   <span className="field-name" style={{ marginLeft: "-139px" }}>
@@ -431,62 +351,172 @@ const Dashboard = () => {
                   />
                 </div>
               </div>
-            </div> */}
+            </div>
 
             <div className="list-view-section">
               <span className="list-view-textD">
                 CARD VIEW ({filteredMatches.length})
               </span>
             </div>
-            <div className="card-container-main">
-            {filteredMatches.map((match, index) => (
-                <div className='card-container'  key={index}>
-                <div className='match-number'> MATCH {match.m5MatchNo}, {match.m5GenderName?.toUpperCase()}
+            <div class="card-container">
+              {filteredMatches.map((match, index) => (
+                <div
+                  key={index}
+                  className="card"
+                  style={{ height: "346px", width: "350px" }}
+                >
+                  <div className="left-section">
+                    MATCH {match.m5MatchNo}, {match.m5GenderName?.toUpperCase()}
                     ,{" "}
                     {match.m5CompetitionName
                       ? match.m5CompetitionName.substring(0, 21).trim()
-                      : ""}</div>
-                <div className='match-name'>
-                    <div className='bpl-className'>
-                        <div className='bpl-name-time'>
-                            <div className='bpl-name'>{match.m5SeriesName}</div>
-                            <div className='date-time'>{match.m5StartDate?.split("T")[0]},{" "}
-                            {match.m5MatchStartTimeLocal} (Local)</div>
-                            <div className='stadium-name'>{match.m5GroundName}</div>
-                        </div>
-                        <div className='firstClass'><div className='firstclass-name'>{match.m5MatchFormat}</div></div>
+                      : ""}
+                  </div>
+                  <div className="main-content-card" style={{ height: "100%" }}>
+                    <div
+                      className="header-card content"
+                      style={{
+                        marginBottom: "0px",
+                        borderBottomWidth: "0px",
+                        borderBottomStyle: "solid",
+                        height: "35%",
+                        marginTop: "0px",
+                      }}
+                    >
+                      <div className="row">
+                        <h5>{match.m5SeriesName}</h5>
+                        <br />
+                        <p className="sub-text">
+                          {match.m5StartDate?.split("T")[0]},{" "}
+                          {match.m5MatchStartTimeLocal} (Local)
+                          <br />
+                          {match.m5GroundName}
+                        </p>
+                      </div>
+
+                      <p className="sub-text"></p>
+                      <div
+                        className="first-class"
+                        style={{
+                          marginLeft: "0px",
+                          left: "227.467px",
+                          right: "0px",
+                          borderLeftWidth: "0px",
+                          borderLeftStyle: "solid",
+                          paddingLeft: "5px",
+                          bottom: "0px",
+                          top: "0px",
+                          marginTop: "0px",
+                          borderTopWidth: "0px",
+                          borderTopStyle: "solid",
+                        }}
+                      >
+                        {match.m5MatchFormat}
+                      </div>
                     </div>
-                    <div className='team-name-time'>
-                        <div className='team1Name-img'>
-                            <div><img style={{width:"41px"}} src={match.m5TeamALogo} alt={match.m5TeamA} /></div>
-                            <div>{match.m5TeamAShortName}</div>
+
+                    <div
+                      className="content"
+                      style={{ marginTop: "0px", height: "35%" }}
+                    >
+                      <div
+                        className="team-section"
+                        style={{
+                          borderBottomWidth: "0px",
+                          borderBottomStyle: "solid",
+                          paddingBottom: "0px",
+                          paddingTop: "0px",
+                        }}
+                      >
+                        <div className="team">
+                          <img src={match.m5TeamALogo} alt={match.m5TeamA} />
+                          <p>{match.m5TeamAShortName}</p>
                         </div>
-                        <div className='matchTime'>
-                            <div>Starting in</div>
-                            <div>{match.m5MatchStartTimeLocal}</div>
+                        <div
+                          className="timer"
+                          style={{
+                            paddingLeft: "5px",
+                            borderLeftWidth: "0px",
+                            borderLeftStyle: "solid",
+                            marginLeft: "30px",
+                            marginRight: "30px",
+                          }}
+                        >
+                          {/* {match.m5MatchStatus === "Live" ? "Live Now" : `Starting at ${match.m5MatchStartTimeLocal}`} */}
+                          Starting in {match.m5MatchStartTimeLocal}
                         </div>
-                        <div className='team2Name-img'>
-                            <div><img style={{width:"41px"}} src={match.m5TeamBLogo} alt={match.m5TeamB} /></div>
-                            <div>{match.m5TeamBShortName}</div>
+                        <div className="team">
+                          <img src={match.m5TeamBLogo} alt={match.m5TeamB} />
+                          <p>{match.m5TeamBShortName}</p>
                         </div>
+                      </div>
                     </div>
-                    <div className='odds-review-operator'>
-                        <div className='odds-review-operator-name'>
-                            <div className='odds-review'>
-                                <div className='odds'>ODDS</div>
-                                <div className='review-name'>REVIEW</div>
-                            </div>
-                            <div className='operator-name'><div>OPERATOR NAME</div><img src={img4}/></div>
+
+                    <div className="footer">
+                      <div className="footer-top" style={{ height: "50%" }}>
+                        <div
+                          style={{
+                            paddingRight: "0px",
+                            paddingLeft: "0px",
+                            marginRight: "-97px",
+                            marginLeft: "-40px",
+                          }}
+                        >
+                          ODDS
                         </div>
-                        <div className='menubar'><div className='menubar-img'><img src={img3}/></div></div>
+                        <div
+                          style={{ paddingLeft: "0px", paddingRight: "0px" }}
+                        >
+                          REVIEW
+                        </div>
+                      </div>
+                      <div
+                        className="footer-bottom"
+                        style={{
+                          paddingLeft: "0px",
+                          paddingRight: "75px",
+                          marginLeft: "0px",
+                          left: "0px",
+                          height: "50%",
+                          cursor: "pointer",
+                        }}
+                        onClick={() => navigate("/assign_match")}
+                      >
+                        OPERATOR NAME{" "}
+                        <img
+                          src="../images/dashboard_main/arrow_down.png"
+                          alt="Arrow Down"
+                        />
+                        <div
+                          className="ticket"
+                          style={{
+                            left: "226.983px",
+                            right: "0px",
+                            borderTopWidth: "0px",
+                            borderTopStyle: "solid",
+                            paddingTop: "23px",
+                            paddingBottom: "0px",
+                            borderBottomWidth: "0px",
+                            borderBottomStyle: "solid",
+                            marginTop: "0px",
+                            top: "-52px",
+                            marginBottom: "-10px",
+                          }}
+                        >
+                          <img
+                            src="../images/dashboard_main/Group 1829.png"
+                            alt="Group 1829"
+                            style={{
+                              marginTop: "10px", // Adjust value as needed to move the image down
+                            }}
+                          />
+                        </div>
+                      </div>
                     </div>
+                  </div>
                 </div>
-                </div>
-            ))}
+              ))}
             </div>
-           
-            
-            
             {/* Pagination Controls */}
             {filteredMatches.length > 0 && (
               <div className="pagination d-flex justify-content-center align-items-center mt-3">
